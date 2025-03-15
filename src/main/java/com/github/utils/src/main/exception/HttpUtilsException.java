c1d49b839c140cd2181f908ad24e97e47f96fc3e
Abhishek Mallikarjuna <abhishek.mallikarjuna@speridian.com>
2025-03-16 00:33:43 +0530

    feat(utils): add http utils

---
+++ src/main/java/com/github/utils/src/main/exception/HttpUtilsException.java
@@ -0,0 +1,26 @@
+package com.github.utils.src.main.exception;
+
+/**
+ * Exception thrown by HttpUtils when an error occurs.
+ */
+public class HttpUtilsException extends RuntimeException {
+
+    /**
+     * Creates a new HttpUtilsException with the specified message.
+     *
+     * @param message the message
+     */
+    public HttpUtilsException(String message) {
+        super(message);
+    }
+
+    /**
+     * Creates a new HttpUtilsException with the specified message and cause.
+     *
+     * @param message the message
+     * @param cause the cause
+     */
+    public HttpUtilsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
